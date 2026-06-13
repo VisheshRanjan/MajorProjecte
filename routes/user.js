fb82a66ebb0b767ed57475b7eaa1fb0f567b98d0
Vishesh Ranjan <visheshranjan5259@gmail.com>
2026-06-13 23:57:06 +0530

    Bugs Fixed

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router({mergeParams:true});
+const User = require("../models/user.js");
+const passport = require("passport");
+
 
 router.get("/signUp",(req,res)=>{
     res.render("../views/signUp.ejs");
 });
 
 router.post("/signUp",async(req,res)=>{
-    let
+    try {
+        let {username, email, password} = req.body;
+        const newUser = new User({username, email});
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        res.redirect("/listings");
+    } catch (e) {
+        console.error(e);
+        res.redirect("/signUp");
+    }
 });
 
 module.exports= router;
